30f2f8ed90fcb5737feef308ddb97c5c284ea74e
Pallavi-2410 <patel.pallavi1819@gmail.com>
2024-12-28 13:47:19 +0530

    Complete Quiz App

--- scripts/question.js
+++ scripts/question.js
@@ -1,25 +1,63 @@
 import { baseUrl } from "./baseUrl.js";
 
-function fetchReviewedQuestion() {
-    fetch(`${baseUrl}`)
-        .then(res => res.json())
-        .then(data => {
-            let grid = document.getElementById("reviewedQuestionGrid")
-            grid.innerHTML = "";
-            let reviewedQuestion = data.filter(questions => questions.reviewStatus == true)
-            reviewedQuestion.forEach(questions => {
-                let card = document.createElement("div")
-                card.className = "question-card"
-                card.style.borderColor = "violet"
-                card.innerHTML = `
-            <h3>${questions.title}</h3>
-            <h3>${questions.optionA}</h3>
-            <h3>${questions.optionB}</h3>
-            <h3>${questions.optionC}</h3>
-            <h3>${questions.optionD}</h3>
-            `
-            grid.appendChild(card)
-            });            
+window.onload = () => {
+    getData();
+};
+
+function getData() {
+    fetch(`${baseUrl}/questions`)
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data);
+            displayData(data);
+        })
+        .catch((err) => {
+            alert("something went wrong");
+            console.log(err);
         });
 }
-fetchReviewedQuestion()
+
+function displayData(arr) {
+    let cont = document.getElementById("cont");
+    cont.innerHTML = "";
+    let newArr = arr.filter((el, i) => el.reviewStatus == true)
+    newArr.map((el, i) => {
+        let card = document.createElement("div");
+        let title = document.createElement("h4");
+        title.textContent = `Title: ${el.title}`;
+
+        let optionA = document.createElement("h4");
+        optionA.textContent = `A: ${el.optionA}`;
+
+        let optionB = document.createElement("h4");
+        optionB.textContent = `B: ${el.optionB}`;
+
+        let optionC = document.createElement("h4");
+        optionC.textContent = `C: ${el.optionC}`;
+
+        let optionD = document.createElement("h4");
+        optionD.textContent = `D: ${el.optionD}`;
+
+        let correctOption = document.createElement("h4");
+        correctOption.textContent = `Correct Option: ${el.correctOption}`;
+
+        let reviewStatus = document.createElement("h4");
+        if (el.reviewStatus == true) {
+            reviewStatus.textContent = "Status: Reveiwed";
+        } else {
+            reviewStatus.textContent = "Status: Pending";
+        }
+        card.append(
+            title,
+            optionA,
+            optionB,
+            optionC,
+            optionD,
+            correctOption,
+            reviewStatus,
+        );
+        cont.append(card);
+})        
+    };
+
+
